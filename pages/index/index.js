990b035dad4c089672e58c895773e2e58ab33f67
Claire LI <lijiaoni019@163.com>
2020-08-04 21:46:25 +0800

    post page init

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,29 @@
 Page({
   getDataFromBaasPost: function () {
+    let Bars = new wx.BaaS.TableObject("bar")
+    let query = new wx.BaaS.Query()
+    Bars.setQuery(query).orderBy('created_at').find().then (res => {
+      console.log(res)
+      let bars = res.data.objects
+      this.setData({bars})
+      console.log(this.data)
+    
+    })
+
+  },
+
+  navigateToShowPage:function(e){
+    console.log(e)
+    let id = e.currentTarget.dataset.id;
+    wx.navigateTo({
+      url: `../show/show?id=${id}`,
+    })
 
   },
   
   onLoad: function () {
+    this.getDataFromBaasPost()
+
 
   },
 
